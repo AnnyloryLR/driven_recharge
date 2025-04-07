cd8487092d60c3f49fd019ec828444df66e9c4a0
Annylory LR <annyloryrosa@gmail.com>
2025-04-07 09:24:01 -0300

    summary done

--- src/repositories/summary-repository.ts
+++ src/repositories/summary-repository.ts
@@ -1,8 +1,25 @@
 import db from '../config/database';
 
 export async function getSummary(cpf:string){
-    const result = await db.query(`
-        SELECT * FROM phones WHERE cpf= $1`,[cpf]);
+    const result1 = await db.query(`
+        SELECT * FROM phones WHERE cpf= $1;`,[cpf]);
+    
+    const carrier_name = result1.rows[0].carrier; 
+    
+    const result2 = await db.query(`
+        SELECT * FROM carriers WHERE name= $1;`,[carrier_name]);
+    
+    const id = result1.rows[0].id;
+    
+    const result3 = await db.query(`
+        SELECT * FROM recharges WHERE phone_id= $1;`,[id]);
+        
+    const result = {
+        document:result1.rows[0].cpf,
+        phones: result1.rows,
+        carrier:result2.rows[0],
+        recharges:result3.rows
+        };
 
-    console.log(result.rows);
+    return result
 }
