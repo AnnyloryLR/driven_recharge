05a1dbb0fbdf8a89663753e84921697681cf3558
Annylory LR <annyloryrosa@gmail.com>
2025-04-10 15:58:18 -0300

    problems in compilation

--- dist/routers/summary-router.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var summary_controller_1 = require("../controllers/summary-controller");
-var summaryRouter = (0, express_1.Router)();
-summaryRouter.get("/summary/:document", summary_controller_1.getSummary);
-exports.default = summaryRouter;
